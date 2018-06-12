2205f1d4091406f0de08081b2a9573a12e6f70ac
popwandee <popwandee@gmail.com>
2018-06-12 16:53:26 +0700

    update call back

--- callback.php
+++ callback.php
@@ -16,5 +16,18 @@
  * under the License.
  */
 
+use LINE\LINEBot\KitchenSink\Dependency;
+use LINE\LINEBot\KitchenSink\Route;
+use LINE\LINEBot\KitchenSink\Setting;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$setting = Setting::getSetting();
+$app = new \Slim\App($setting);
+
+(new Route())->register($app);
+(new Dependency())->register($app);
+
+$app->run();
 
 echo "hello world this is callback.php";
