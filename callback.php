e23398a5b348c2e3ecd7ae0a38aea317d94ae2c3
popwandee <popwandee@gmail.com>
2018-06-12 17:09:40 +0700

    test

--- callback.php
+++ callback.php
@@ -20,7 +20,7 @@ use LINE\LINEBot\KitchenSink\Dependency;
 use LINE\LINEBot\KitchenSink\Route;
 use LINE\LINEBot\KitchenSink\Setting;
 
-echo "hello world this is before callback.php";
+echo "hello world this is before the first callback.php";
 require_once __DIR__ . 'vendor/autoload.php';
 
 echo "hello world this is middle callback.php";
