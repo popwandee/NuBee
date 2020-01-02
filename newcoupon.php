2867ba07af422703cb9d72d9aa2a7a67a25509dc
popwandee <popwandee@gmail.com>
2020-01-02 16:56:46 +0700

    Create newcoupon.php

---
+++ newcoupon.php
@@ -0,0 +1,74 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>ระบบการจ่ายคูปองค่าอาหารกลางวัน พัน.ขกท.</title>
+      
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+          
+</head>
+<body>
+  
+    <!-- container -->
+    <div class="container">
+   
+        <div class="page-header">
+            <h1>ลงทะเบียนรับคูปอง</h1>
+        </div>
+      
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+    <table class='table table-hover table-responsive table-bordered'>
+        <tr>
+            <td>ยศ ชื่อ สกุล</td>
+            <td><input type='text' name='name' class='form-control' /></td>
+        </tr>
+        <tr>
+            <td>รหัสประจำตัวข้าราชการทหาร</td>
+            <td><input type='text' name='government_id' class='form-control' /></td>
+        </tr>
+        <tr>
+            <td>รหัสคูปอง</td>
+            <td><input type='text' name='coupon_id' class='form-control' /></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <input type='ยืนยัน' value='Save' class='btn btn-primary' />
+                <a href='index.php' class='btn btn-danger'>Back to read products</a>
+            </td>
+        </tr>
+    </table>
+</form>
+          
+    </div> <!-- end .container -->
+      
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+   
+<!-- Latest compiled and minified Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+</body>
+</html>
+<?php
+
+if($_POST){
+ define("MLAB_API_KEY", '6QxfLc4uRn3vWrlgzsWtzTXBW7CYVsQv');
+   try{
+    $newCoupon = json_encode(array('name'=> $name,'government_id'=> $government_id,'coupon_id'=> $coupon_id,'log_time'=> $timeNow) );
+                              $opts = array('http' => array( 'method' => "POST",
+                                          'header' => "Content-type: application/json",
+                                          'content' => $newCoupon
+                                           )
+                                        );
+           
+                              $url = 'https://api.mlab.com/api/1/databases/nubee/collections/coupon?apiKey='.MLAB_API_KEY.'';
+                              $context = stream_context_create($opts);
+                              $returnValue = file_get_contents($url,false,$context);
+     }
+    // show error
+    catch(PDOException $exception){
+        die('ERROR: ' . $exception->getMessage());
+    }
+}
+?>
