4efadf9fee678046451af4348fd0a93baa788894
popwandee <popwandee@gmail.com>
2020-01-03 20:40:40 +0700

    Update listcoupon.php

--- listcoupon.php
+++ listcoupon.php
@@ -20,7 +20,7 @@
 $tz_object = new DateTimeZone('Asia/Bangkok');
          $datetime = new DateTime();
          $datetime->setTimezone($tz_object);
-         $dateTimeToday = $datetime->format('Y\-m\-d\');
+         $dateTimeToday = $datetime->format('Y-m-d');
 	 if(!isset($dateGetCoupon)){$dateGetCoupon=$dateTimeToday;}
 	?>
 	
