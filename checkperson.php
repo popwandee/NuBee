8fac58ed3b6a547b13ec229300065a85110df4ca
popwandee <popwandee@gmail.com>
2020-01-03 16:07:28 +0700

    Create checkperson.php

---
+++ checkperson.php
@@ -0,0 +1,7 @@
+<?php
+if($_POST){
+        $name=htmlspecialchars(strip_tags($_POST['name']));
+        $government_id=htmlspecialchars(strip_tags($_POST['government_id']));
+        $coupon_id=htmlspecialchars(strip_tags($_POST['coupon_id']));
+        }
+?>
