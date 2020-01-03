b587c4e16dcdeedadbf8a22e635255baa842801c
popwandee <popwandee@gmail.com>
2020-01-03 20:38:17 +0700

    Update newcoupon.php

--- newcoupon.php
+++ newcoupon.php
@@ -18,7 +18,7 @@
 <?php
 define("MLAB_API_KEY", '6QxfLc4uRn3vWrlgzsWtzTXBW7CYVsQv');
 $tz_object = new DateTimeZone('Asia/Bangkok');
-$datetime = new DateTime(); $datetime->setTimezone($tz_object); $dateTimeToday = $datetime->format('Y\-m\-d\');
+$datetime = new DateTime(); $datetime->setTimezone($tz_object); $dateTimeToday = $datetime->format('Y-m-d');
 
 
 ?>
