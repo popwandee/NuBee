fdddfa6c96f42a33c708c2bbc537e886d60259cf
popwandee <popwandee@gmail.com>
2020-01-03 16:08:47 +0700

    Update checkperson.php

--- checkperson.php
+++ checkperson.php
@@ -4,4 +4,7 @@ if($_POST){
         $government_id=htmlspecialchars(strip_tags($_POST['government_id']));
         $coupon_id=htmlspecialchars(strip_tags($_POST['coupon_id']));
         }
+echo "NAME :".$name;
+echo "Government ID :".$government_id;
+echo "COUPON ID :".$coupon_id;
 ?>
