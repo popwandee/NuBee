1a2374ef8ba6d5dc8c242a1209dcc1da94825b4b
popwandee <popwandee@gmail.com>
2020-01-03 16:54:35 +0700

    Create search.php

---
+++ search.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>ระบบการจ่ายคูปองค่าอาหารกลางวัน พัน.ขกท.</title>
+      
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+          
+</head>
+<body>
+  
+    <!-- container -->
+    <div class="container">
+   
+        <div class="page-header">
+            <h1>ลงทะเบียนรับคูปอง</h1>
+        </div>
+      
+    <form action="newcoupon.php" method="post">
+    <table class='table table-hover table-responsive table-bordered'>
+        
+        <tr>
+            <td>รหัสประจำตัวข้าราชการทหาร</td>
+            <td><input type='text' name='government_id' class='form-control' /></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <input type='submit' value='Search' class='btn btn-primary' />
+            </td>
+        </tr>
+    </table>
+</form>
+          
+    </div> <!-- end .container -->
+    
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+   
+<!-- Latest compiled and minified Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+</body>
+</html>
