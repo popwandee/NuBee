b242762bc1e27d379587f76eecc01ac1b387d0cf
popwandee <popwandee@gmail.com>
2020-01-04 08:11:26 +0700

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,37 @@
+<?php
+// include database connection
+include 'config.php';
+ 
+try {
+     
+    // get record ID
+    // isset() is a PHP function used to verify if a value is there or not
+    $id=isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
+ 
+    // delete query
+    
+$opts = array('http' => array( 'method' => "DELETE",
+                               'header' => "Content-type: application/json",
+                                           )
+                                        );
+$url = 'https://api.mlab.com/api/1/databases/nubee/collections/coupon/'.$_id.'?apiKey='.MLAB_API_KEY.',type: “DELETE”,
+   timeout: 300000';
+        $context = stream_context_create($opts);
+        $returnValue = file_get_contents($url,false,$context);
+        if($returnValue){
+		  // redirect to read records page and 
+        // tell the user record was deleted
+        header('Location: listcoupon.php?action=deleted');
+	        }else{
+		    die('Unable to delete record.');
+                 }
+	
+     
+   
+}
+ 
+// show error
+catch(PDOException $exception){
+    die('ERROR: ' . $exception->getMessage());
+}
+?>
