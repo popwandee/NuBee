3b5e22a32e4d047eebdf07a5703d81759c494c8c
popwandee <popwandee@gmail.com>
2020-01-05 09:26:41 +0700

    Update nubee.php

--- nubee.php
+++ nubee.php
@@ -579,8 +579,6 @@ if(!is_null($events)){
 		                       $multiMessage->add($textMessage);
 	                               }
 			
-				       $textMessage = new TextMessageBuilder($textReplyMessage);
-		                       $multiMessage->add($textMessage);
 				       $replyData = $multiMessage;
 
                                  break;		
@@ -588,7 +586,7 @@ if(!is_null($events)){
 				if(!isset($explodeText[2])){
 					$explodeText[2] = $datetime->format('Y-m-d');
 				}
-				 $json = file_get_contents('https://api.mlab.com/api/1/databases/nubee/collections/coupon?apiKey='.MLAB_API_KEY.'&q={"$and":[{"coupon":"'.$explodeText[1].'"},{"dateGetCoupon":{"$regex":"'.$explodeText[2].'"}}]}');
+				 $json = file_get_contents('https://api.mlab.com/api/1/databases/nubee/collections/coupon?apiKey='.MLAB_API_KEY.'&q={"coupon":"'.$explodeText[1].'","dateGetCoupon":{"$regex":"'.$explodeText[2].'"}}');
                                      $data = json_decode($json);
                                      $isData=sizeof($data);
 
