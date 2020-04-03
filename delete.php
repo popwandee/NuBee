00ada6a1f04b809661cb7f4ede71007a1813fbfc
popwandee <popwandee@gmail.com>
2020-04-03 15:52:07 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -9,21 +9,20 @@ try {
     $id=isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
  
     // delete query
-    echo "$id is ".$id;
+
 $opts = array('http' => array( 'method' => "DELETE",
                                'header' => "Content-type: application/json",
                                            )
                                         );
-$url = 'https://api.mlab.com/api/1/databases/nubee/collections/coupon/'.$id.'?apiKey='.MLAB_API_KEY.',type: "DELETE",
-   timeout: 300000';
+$url = 'https://api.mlab.com/api/1/databases/nubee/collections/coupon/'.$id.'?apiKey='.MLAB_API_KEY.',type:DELETE,timeout: 300000';
         $context = stream_context_create($opts);
         $returnValue = file_get_contents($url,false,$context);
         if($returnValue){
 		  // redirect to read records page and 
-        // tell the user record was deleted
-        header('Location: listcoupon.php?message=deleted');
+        	// tell the user record was deleted
+       		 header('Location: listcoupon.php?message=deleted');
 	        }else{
-		    die('CannotDeleted');
+		    header('Location: listcoupon.php?message=CannotDeleted');
                  }
 	
      
