974f20c9e79b59a604c8489d178b9726a15af375
popwandee <popwandee@gmail.com>
2020-04-03 16:22:39 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -14,7 +14,7 @@ $opts = array('http' => array( 'method' => "DELETE",
                                'header' => "Content-type: application/json",
                                            )
                                         );
-$url = 'https://api.mlab.com/api/1/databases/nubee/collections/coupon/'.$id.'?apiKey='.MLAB_API_KEY.';
+$url = 'https://api.mlab.com/api/1/databases/nubee/collections/coupon/'.$id.'?apiKey=MLAB_API_KEY;
         $context = stream_context_create($opts);
         $returnValue = file_get_contents($url,false,$context);
         if($returnValue){
