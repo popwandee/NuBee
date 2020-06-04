a6f1fa9072ca932bdb0ac77108001805403424db
popwandee <popwandee@gmail.com>
2020-06-04 18:03:26 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define("MLAB_API_KEY", '6QxfLc4uRn3vWrlgzsWremovemetzTXBW7CYVsQv');
+define("MLAB_API_KEY", '');
 $tz_object = new DateTimeZone('Asia/Bangkok');
 $datetime = new DateTime(); $datetime->setTimezone($tz_object); $dateTimeToday = $datetime->format('Y-m-d');
 ?>
