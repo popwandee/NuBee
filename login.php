a3b014a071f4bf86f2c31878788fa8ca62c7d9e8
popwandee <popwandee@gmail.com>
2020-12-16 22:02:49 +0700

    Update login.php

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
  
 // Include config file
 require_once "config.php";
-include("vender/restdbclass.php");
+include "vender/restdbclass.php";
  
 // Define variables and initialize with empty values
 $username = $password = "";
